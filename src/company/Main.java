1b6e015913a48c2e9d516c93c3d8709ec844eee3
iuri <iuricube@gmail.com>
2020-11-25 21:43:41 +0100

    added more conditions to ensure the creature is dead in menu()

--- src/company/Main.java
+++ src/company/Main.java
@@ -12,9 +12,16 @@ public class Main {
     }
 
     public static void menu() {
+        if (!miaCreatura.isSonoVivo()){
+            System.out.println(miaCreatura.getNome() + " e morto/a");
+            return;
+        }
         System.out.println("\ncosa vuoi fare a " + miaCreatura.getNome() + "\n1 per lavorare\n2 per mangiare\n3 per lavare\n4 per giocare\n5 per medicare\n0 per uscire");
         switch (in.nextInt()) {
-            case 0 -> { return; }
+            case 0 -> {
+                System.out.println("sto uscendo");
+                return;
+            }
             case 1 -> miaCreatura.faiLavoro();
             case 2 -> miaCreatura.daiCibo();
             case 3 -> miaCreatura.faiBagnetto();
@@ -26,7 +33,7 @@ public class Main {
     }
 
     public static String scegliNome() {
-        System.out.println("Benvenuto nel gioco del Tamagotchi\n");
+        System.out.println("\nBenvenuto nel gioco del Tamagotchi\n");
         System.out.println("Scegli il nome della tua creatura");
         return in.nextLine();
     }
